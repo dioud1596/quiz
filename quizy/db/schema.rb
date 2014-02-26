bcb7fbd4485e53f93a7174c59423526751c42183
Edouard <edouard.rudolf@gmail.com>
2014-02-26 09:27:19 +0100

    admin column and pundit

--- quizy/db/schema.rb
+++ quizy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225133708) do
+ActiveRecord::Schema.define(version: 20140225174311) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140225133708) do
     t.date     "birth_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
